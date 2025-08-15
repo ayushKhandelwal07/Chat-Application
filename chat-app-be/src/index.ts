d7f3767662940c68d3771a3fcdacc02a51ad0c54
ayushKhandelwal07 <ayushkhandelwal227@gmail.com>
2025-08-15 13:28:45 +0530

    be_donw

--- chat-app-be/src/index.ts
+++ chat-app-be/src/index.ts
@@ -1,65 +1,237 @@
 import { WebSocket, WebSocketServer } from "ws";
-
+import { randomBytes } from 'crypto';
 
 const wss = new WebSocketServer({port : 8080});
 
-
-interface User {
-        socket : WebSocket;
-        roomId : string;
-        type ?: "public" | "private"; 
+interface Messages {
+    content : string;
+    sender : string;
 }
 
+// interface JoinMessage {
+//     type : "join",
+//     roomType : "public" | "private", 
+//     payload : {
+//         user : string;
+//         roomId ?: string;
+//         createCode ?: boolean;
+//     };
+// };
 
-const all_websockets : User[] = []; 
+// interface JoinChat {
+//     roomType : "public | private",
+//     type : "chat",
+//     payload : {
+//         message : string;
+//     }
+// }
+
+interface RoomData {
+    users : Map<WebSocket , string>;  // Map <curr_websocket_of_user && , username>
+    messages : Messages[];
+    lastActive : number;
+    roomType : "public" | "private"; 
+}
+
+const rooms = new Map<string | "publicId", RoomData>();  // Map<roomId , roomData>
+const users = new Map<WebSocket , string>();           // Map <curr_websocket_of_user && , username>
 
 
 wss.on("connection", (socket) => {
+    
+    socket.on("message", (message : string) => {
+
+    try{
+
+        const data  = JSON.parse(message);
 
 
-        socket.on("message" ,(message : string)  => {
+        //all logic for the pubilc room join 
+        if(data.roomType === "public"){
+            if(data.type === "join"){
+                const username = data.payload.username;
+
+                users.set(socket,username);
+
+                if(!rooms.get("publicId")){
+                    rooms.set("publicId",{
+                        users : new Map([[socket, username]]),
+                        messages : [],
+                        lastActive : Date.now(),
+                        roomType : "public"
+                    });
+                }else{
+                    const publicRoom = rooms.get("publicId")!;
+                    publicRoom.users.set(socket, username);
+                    publicRoom.lastActive = Date.now();
+                }
+
+
+                socket.send(JSON.stringify({
+                    type : "joinConfirmation",
+                    roomType : "public",
+                    payload : {
+                        username : username,
+                        timestamp : Date.now()
+                    }
+                }));
+            };
+
+
+        
+            if(data.type === "chat"){
+                const username = users.get(socket);
+
+                if(!username){
+                    socket.send(JSON.stringify({
+                        type : "err",
+                        message : "User not found"
+                    }));
+                    return;
+                }
+
+                const message  = {
+                    content : data.payload.message,
+                    sender : username
+                }
+
+                const publicRoom = rooms.get("publicId");
                 
-                try {
-                        const data = JSON.parse(message);
-                        
-                        if(data.type === "join"){
-                                all_websockets.push({
-                                socket , 
-                                roomId :  data.payload.roomId
-                        });
-                }
+                if(publicRoom){
+                    publicRoom.messages.push(message);
 
-                if(data.type === "chat"){
-                        const currentUserRoomId = all_websockets.find((x) => x.socket === socket)?.roomId;
+                    publicRoom.users.forEach((_,userSocket) => {
+                        if(userSocket !== socket){
+                            userSocket.send(JSON.stringify({
+                            type : "chat",
+                            roomType : "public",
+                            payload : {
+                                message : message.content,
+                                sender : message.sender,
+                                timestamp : Date.now()
+                            }
+                        }));
+                    }});
+                };
+            };
+        
+        };
 
-                        if(!currentUserRoomId) {
-                                socket.send(JSON.stringify({error: "User not in any room"}));
-                                return;
+        // all logic for the || private room || join code 
+        if(data.roomType === "private"){
+            if(data.type === "join"){
+                if(data.gencode){
+                    const roomCode = randomBytes(3).toString('hex').toUpperCase();
+                    const username = data.payload.username;
+
+                    users.set(socket, username);
+
+                    rooms.set(roomCode , {
+                        users : new Map([[socket,username]]),
+                        messages : [],
+                        lastActive : Date.now(),
+                        roomType : "private"
+                    });
+
+                    socket.send(JSON.stringify({
+                        type : "roomCreated",
+                        roomType : "private",
+                        payload : {
+                            roomCode : roomCode,
+                            username : username,
+                            timestamp : Date.now()
                         }
-                        
-                        const currRoomUsers = all_websockets.filter((x) => {
-                                return x.roomId === currentUserRoomId;
-                        });
-                        
-                        currRoomUsers.forEach((x) => {
-                                if(x.socket.readyState === WebSocket.OPEN && x.socket != socket) {
-                                        x.socket.send(JSON.stringify({
-                                                type: "chat",
-                                                message: data.payload.message
-                                        }));
+                    }));
+
+                }else{
+
+                    const roomId = data.payload.roomId;
+                    const username = data.payload.username;
+
+                    const privateRoom = rooms.get(roomId);
+                    if(privateRoom){
+                        privateRoom.users.set(socket,username);
+                        users.set(socket,username);
+
+                        socket.send(JSON.stringify({
+                            type : "joinConfirmation",
+                            roomType : "private",
+                            payload : {
+                                roomId : roomId,
+                                username : username,
+                                timestamp : Date.now()
+                            }
+                        }));
+                    }else{
+                        socket.send(JSON.stringify({
+                            type : "err",
+                            message : "room not found"
+                        }))
+                    }
+
+                }
+            }
+        
+            if(data.type === "chat"){
+                const username = users.get(socket);
+                const roomId = data.payload.roomId;
+
+                if(!username){
+                    socket.send(JSON.stringify({
+                        type : "err",
+                        message : "User not found"
+                    }));
+                    return;
+                }
+
+                const message = {
+                    content : data.payload.message,
+                    sender : username
+                }
+
+                const privateRoom = rooms.get(roomId);
+                if(privateRoom){
+                    privateRoom.messages.push(message);
+
+                    privateRoom.users.forEach((_ , userSocket) => {
+                        if(userSocket !== socket){
+                            userSocket.send(JSON.stringify({
+                                type : "chat",
+                                roomType : "private",
+                                payload : {
+                                    message : message.content,
+                                    sender : message.sender,
+                                    timestamp : Date.now()
                                 }
-                        });
+                            }));
+                        }
+                    })
                 }
+            
+            }
+        }
 
-                } catch (error) {
-                        console.error("Error parsing message:", error);
-                        socket.send(JSON.stringify({error: "Invalid message format"}));
-                        return;
+    }catch(err){
+        console.log("JSON parse Error : ",err);
+        socket.send(JSON.stringify({
+            type : "error",
+            message : "Invalid message format"
+        }))
+    }
+}); 
+
+
+    socket.on("close", () => {
+        users.delete(socket);
+    
+        rooms.forEach((room, roomId) => {
+            if (room.users.has(socket)) {
+                    room.users.delete(socket);
+    
+                if (room.users.size === 0 && roomId !== "publicId") {
+                    rooms.delete(roomId);
                 }
-        });     
-
-
-        socket.on("close", () => {
-                all_websockets.filter(x => x.socket != socket);
-        })
+            }
+        });
+    });             
 });
